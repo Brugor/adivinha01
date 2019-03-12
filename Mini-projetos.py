55f4fecee32c7cfd070e80ad3a1f3f28e17d1e72
Brugor <44418361+Brugor@users.noreply.github.com>
2019-03-12 22:57:17 +0000

    adivinha02
    Acrescentando novas funcionalidades ao jogo.

--- Mini-projetos.py
+++ Mini-projetos.py
@@ -4,12 +4,28 @@ import random
 #Geremos um número secreto entre 1 e 100. Para randrange(a, b + 1)
 numero_secreto = random.randint(1, 100)
 
-#Número de tentativas que o jogador terá.
-chances = 5
+#Pontos para o placar do jogador
+pontos = 1000
+
+#Escolher o nível de dificuldade
+print("Escolha o nível: (1) Fácil, (2) Médio, (3) Difícil")
+nivel = int(input("Nível: "))
+
+#As chances que o jogador terá de acordo com o nível escolhido
+if(nivel == 1):
+    chances = 8
+elif(nivel == 2):
+    chances = 6
+elif(nivel == 3):
+    chances = 4
+elif(nivel < 1 or nivel > 3):
+    print("Valor {} não é um nível válido.".format(nivel))
+    chances = 0
+
 
 #O jogador continuará  "Enquanto" (while) as chances forem maiores que zero
 while chances > 0:
-    #Informação sobre a quantidade de chances e o input, transformado em número por int()
+    #Informação sobre a quantidade de chances e o input, transformado string em número por int()
     print("Tem {} chances" .format(chances))
     chute = int(input("Chute um número de 1 a 100: "))
 
@@ -20,23 +36,34 @@ while chances > 0:
         menor = chute < numero_secreto
         igual = chute == numero_secreto
 
+
+
         #Funções para definir o que fazer em cada situação. O break foi usado em caso de acerto.
-        if(maior):
-            chances = chances - 1
-            print("O número secreto é menor que {}" .format(chute), end="\n \n" )
+        if(igual):
 
-        elif(menor):
-            chances = chances - 1
-            print("O número secreto é maior que {}".format(chute), end="\n \n")
-
-        elif(igual):
             print("Parabéns! Você acertou. O número secreto era {}".format(chute), end="\n \n")
+            print("Você fez {} pontos".format(pontos))
             break
+        #Caso erre as mensagens para cada situação
+        else:
+            if(maior):
+                chances = chances - 1
+                print("O número secreto é menor que {}" .format(chute), end="\n \n" )
+
+            if(menor):
+                chances = chances - 1
+                print("O número secreto é maior que {}".format(chute), end="\n \n")
+
+        #pontos serão retirados a cada jogada.
+        pontos = pontos - (abs(numero_secreto - chute) * chances)
+
+
     #else é executado quando for digitado um valor fora de 1~100, retirando uma chance. o While é reiniciado
     else:
         chances = chances - 1
         print("Você deve digitar um número entre 1 e 100", end="\n \n")
 
-print("Você perdeu! O número secreto era {}".format(numero_secreto))
+if(chances == 0):
+    print("Você perdeu! O número secreto era {}".format(numero_secreto))
 
 
